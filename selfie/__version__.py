5b6269076ed764878d560dc9d13c72e365fd7daf
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2020-10-06 17:59:21 -0400

    make all intermediate directories for --output-file

--- selfie/__version__.py
+++ selfie/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'selfie'
 __description__ = 'Dump environment to JSON file before executing a command'
 __url__ = 'https://github.com/harvard-nrg/selfie'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Neuroinformatics Research Group'
 __author_email__ = 'info@neuroinfo.org'
